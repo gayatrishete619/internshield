b81235b973defe65ac63601f6f737fb90552ec4a
Gayatri Shete <gayatrishete619@gmail.com>
2026-05-03 12:14:18 +0530

    Initial DB setup without exposing credentials

--- DBConnection.java
+++ DBConnection.java
@@ -1,24 +1,24 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.Properties;
+import java.io.FileInputStream;
 
 public class DBConnection {
-
     public static Connection getConnection() {
         Connection con = null;
-
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Properties props = new Properties();
+            props.load(new FileInputStream("db.properties"));
 
-            con = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/internshield",
-                "root",
-                "Gayatri@2006" // change this
-            );
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String pass = props.getProperty("db.password");
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, pass);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return con;
     }
 }
